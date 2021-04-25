7003cc6f2156f846be81813e6945f7ee0c8cecbf
0Nightfall0 <night.tittania@gmail.com>
2021-04-25 03:43:53 -0600

    testing github

---
+++ makefile
@@ -0,0 +1,5 @@
+all:
+
+	g++ main.cpp message.cpp -otry
+	./try
+	#This is to test the makefile
--- message.cpp
+++ message.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 void message::print(){
-	cout<<"Makefile and Software Version Control\n";
+	cout<<"Makefile\n";
 
 }
