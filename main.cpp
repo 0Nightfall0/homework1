39d4c7a9cc8ea8864b74a438f4f6413ec87dd5fb
0Nightfall0 <night.tittania@gmail.com>
2021-04-25 03:04:52 -0600

    testing git

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include "message.h"
+
+using namespace std;
+
+int main()
+{
+
+	message a;
+	a.print();
+
+	return 0;
+}
+
---
+++ message.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "message.h"
+using namespace std;
+
+void message::print(){
+	cout<<"Makefile and Software Version Control\n";
+
+}
